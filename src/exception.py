b1b0fbe15cd5201781ac5253fa1194d0484ec20a
hrishi-DevAI <hrishi2001das@gmail.com>
2024-11-19 12:07:59 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-
+from logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -20,6 +20,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-
     
