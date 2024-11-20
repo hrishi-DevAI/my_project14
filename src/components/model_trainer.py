5a152d8a8962d9de5644a88b14cccbed493c9388
hrishi-DevAI <hrishi2001das@gmail.com>
2024-11-20 10:57:40 +0530

    Hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -41,15 +42,52 @@ class ModelTrainer:
             models = {
                 "Linear Regression": LinearRegression(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Random Forest Regressor": RandomForestRegressor(n_estimators= 500, min_samples_split= 8, max_features= 7, max_depth=8),
-                "XGBRegressor": XGBRegressor(objective='reg:squarederror', reg_lambda= 0.001, booster='gblinear'), 
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(), 
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "Gradient Boosting Regressor": GradientBoostingRegressor()
             }
+            params={
+                "Linear Regression":{},
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],                 
+                    'max_features':['sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models, params=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
